c8ddbd4302e2f8cc7299cfb5609aaab453825a12
Kalashnikov Daniil <kalashnikovdaniil2006@gmail.com>
2025-02-16 21:50:31 +0300

    I am doing some functions and write some sql requests (if build dir )

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/qwertz/Desktop/code/projects/EntryPoint_testing_postgree/FunctionsForCreateAllTables.cpp" "CMakeFiles/app.dir/FunctionsForCreateAllTables.cpp.o" "gcc" "CMakeFiles/app.dir/FunctionsForCreateAllTables.cpp.o.d"
+  "/Users/qwertz/Desktop/code/projects/EntryPoint_testing_postgree/FunctionsForTable_all_people.cpp" "CMakeFiles/app.dir/FunctionsForTable_all_people.cpp.o" "gcc" "CMakeFiles/app.dir/FunctionsForTable_all_people.cpp.o.d"
+  "/Users/qwertz/Desktop/code/projects/EntryPoint_testing_postgree/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/DropAllTable.sql
@@ -0,0 +1,21 @@
+
+DROP TABLE IF EXISTS 
+    Feedback_Participants, 
+    Feedbacks, 
+    People_Subject, 
+    Teacher_Subject, 
+    Students, 
+    Subjects, 
+    Person_Role, 
+    Roles, 
+    People, 
+    Programs, 
+    Tags, 
+    Subject_Tags, 
+    Filters, 
+    Filter_Includes, 
+    Filter_Excludes, 
+    Catagories,
+    Category_Tags, 
+    Person_Filter_Access
+CASCADE;
