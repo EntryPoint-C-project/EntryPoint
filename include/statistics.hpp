063f63b6650b9422c7ad4d54ef76428adf227429
Meytardzhevtd <meytardzhevtd@yandex.ru>
2025-04-10 20:21:38 +0300

    delete all files

--- include/statistics.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef STATISTICS_H
-#define STATISTICS_H
-
-#include "sop.hpp"  // если у вас есть этот файл с описанием структур
-
-std::string generate_statistics(const mtd::Subject& subject);
-
-#endif  // STATISTICS_H
