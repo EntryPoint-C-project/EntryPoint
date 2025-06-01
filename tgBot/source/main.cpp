3b6fcef0ae2e2de91465153ec77ceb481668004d
Meytardzhevtd <meytardzhevtd@yandex.ru>
2025-06-01 18:55:57 +0300

    merge

--- tgBot/source/main.cpp
+++ tgBot/source/main.cpp
@@ -7,6 +7,8 @@
 #include <set>
 #include <thread>
 #include <vector>
+#include <pqxx/pqxx>
+#include <fmt/core.h>
 
 // #include "BotToken.hpp"
 #include "OfficeStaff.hpp"
@@ -16,6 +18,55 @@
 #include "User.hpp"
 #include "sop.hpp"
 #include "statistics.hpp"
+
+// Data Base 
+#include "../../DataBase/include/InteractionsWithTables.hpp"
+#include "../../DataBase/include/TESTS.hpp"
+
+int InitDataBase() {
+    // const std::string conn_str = "dbname=postgres user=postgres password=spelaya_melon hostaddr=127.0.0.1 port=5432";
+    const std::string conn_str = "dbname=ep_db user=danik password=60992425 hostaddr=127.0.0.1 port=5432";
+
+    try {
+        pqxx::connection conn(conn_str);
+        // pqxx::work txn(conn); 
+        if (!conn.is_open()) {
+            throw std::runtime_error("Connection failed");
+        }
+        fmt::print("✓ Подключено к: {}\n", conn.dbname());
+        pqxx::work txn(conn);
+
+
+        DeleteAllTable(txn);
+        CreateAllTable(txn); 
+        TEST_PERSON(txn);
+        TEST_SUBJECT(txn);
+        TEST_PROGRAM(txn);
+        TEST_ROLE(txn);
+        TEST_PERSON_GROUP(txn) ; 
+
+        TEST_PERSON_ROLE(txn);
+        TEST_COURSE(txn);
+        TEST_SUBJECT_OFFER(txn);
+        TEST_TEACGING_ASSIGMENTS(txn);
+
+        TEST_SOP_FORM(txn);
+
+        TEST_GET_ALL_TEACHERS(txn);
+
+        txn.commit();
+
+
+
+
+
+    
+    }catch( const std::exception &e){
+        fmt::print("произошла ошибка : {}\n", e.what());
+    }
+
+    return 0;
+}
 
 mtd::Discipline t;
 mtd::Subject OMP;
@@ -182,6 +233,9 @@ int main() {
     std::map<int64_t, std::shared_ptr<mtd::User>> users;
     std::set<int64_t> NewUsers;
     std::mutex MutexForUsers;
+
+    std::thread t(InitDataBase);
+    t.detach();
 
     bot.getEvents().onCommand(
         "start", [&bot, &users, &MutexForUsers, &NewUsers](TgBot::Message::Ptr message) {
