6951739d42c1755451a2bd223c461b377681d9be
Meytardzhevtd <meytardzhevtd@yandex.ru>
2025-06-13 21:21:41 +0300

    fsdfs

--- Google API/inc/manageSOP.hpp
+++ Google API/inc/manageSOP.hpp
@@ -1,8 +1,8 @@
 #ifndef MANAGESOP_HPP
 #define MANAGESOP_HPP
 
-#include "../new_Version/include/Dop_Functions.hpp"
-#include "../new_Version/include/SOP_Form.hpp"
+#include "../DataBase/include/Dop_Functions.hpp"
+#include "../DataBase/include/SOP_Form.hpp"
 #include "logger.hpp"
 #include <chrono>
 #include <curl/curl.h>
