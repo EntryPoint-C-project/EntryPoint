359d025271f10e60592d4a6f70d82838e146eb13
Meytardzhevtd <meytardzhevtd@yandex.ru>
2025-06-14 00:55:15 +0300

    fdsf

--- tgBot/source/main.cpp
+++ tgBot/source/main.cpp
@@ -280,7 +280,7 @@ TgBot::InlineKeyboardMarkup::Ptr getAdminKeyboard() {
 }
 std::string GetUrlFuckMe_(pqxx::transaction_base& txn, std::string tg_nick) {
     try {
-        std::string sql = "SELECT person_id FROM People WHERE tg_answer = $1";
+        std::string sql = "SELECT person_id FROM People WHERE tg_nick = $1";
         pqxx::result res = txn.exec_params(sql, tg_nick);
         if (res.empty()) {
             fmt::print("Не найден person_id для tg_nick");
