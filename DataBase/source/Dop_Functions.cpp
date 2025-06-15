debe7488d9a7558ebc1d9a59885a80d9903d5eac
Egor <egorlukavenko92@gmail.com>
2025-06-15 22:00:42 +0300

    Update Dop_Functions.cpp

--- DataBase/source/Dop_Functions.cpp
+++ DataBase/source/Dop_Functions.cpp
@@ -61,7 +61,7 @@ void CreatePersonWithParams(pqxx::transaction_base& txn, Person person) {
         // int person_id = CreatePerson(txn , person.first_name , person.last_name , person.tg_nick
         // , person.access , person.snils);
         int person_id;
-        if (IsThereARecordPerson(txn, person.tg_nick)) {
+        if (IsThereARecordPeople(txn, person.tg_nick)) {
             person_id = ReadPersonId(txn, person.tg_nick);
         } else {
             person_id = CreatePerson(txn, person.first_name, person.last_name, person.tg_nick,
