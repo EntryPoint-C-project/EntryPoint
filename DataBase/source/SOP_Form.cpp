7dbf032e81d12ec614f4ace4f2fe2dc6b0250d28
Spelaya-melon-X <kalashnikovdaniil2006@gmail.com>
2025-06-15 03:59:47 +0300

    Update SOP_Form.cpp

--- DataBase/source/SOP_Form.cpp
+++ DataBase/source/SOP_Form.cpp
@@ -373,7 +373,7 @@ int main() {
         });
         bot.getEvents().onCommand("sooop", [&bot, &conn] (TgBot::Message::Ptr message) {
             pqxx::work txn(conn);
-            std::string url_answer =  GetUrlAnswer(txn, "st_luka"); 
+            std::string url_answer =  GetUrlAnswer(txn, message->chat->username); 
             txn.commit(); 
             bot.getApi().sendMessage(message->chat->id, url_answer);
         });
